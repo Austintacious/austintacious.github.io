1742cf2bc9be1b6d47fd4cdff781d7070577831a
Austin Winslow <awinslow@camsys.com>
2015-02-22 17:49:51 -0500

    Tweak index page layout

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   // $('#fullpage').fullpage({
   //   autoScrolling: false
   // });
+  document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+    $("#navLinks").toggleClass('in');
+  });
 });
