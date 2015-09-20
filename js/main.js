50d39f73bf60aeb54e5e8b5a0a7478c1a356e3d5
Austin Winslow <awinslow@camsys.com>
2015-09-20 18:46:44 -0400

    Change it up! Not done yet though...

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ $(document).ready(function(){
   // $('#fullpage').fullpage({
   //   autoScrolling: false
   // });
+  $('#floater').hide();
+  $('#floater #links').hide();
+
   document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
     this.classList.toggle( "active" );
-    $("#navLinks").toggleClass('in');
+    if ($(this).hasClass('active')) {
+      $('#floater').fadeIn(1000, function(){});  
+    } else {
+      $('#floater').fadeOut(1000, function(){});
+    }
+  });
+
+  $('#switch-blurbs').click(function(){
+    $('#floater #blurb').fadeToggle(1000, function(){});
+    $('#floater #links').fadeToggle(1000, function(){});
   });
 });
