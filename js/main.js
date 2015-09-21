76b790ca6ffd0c92214c2839522969c1ba862b9b
Austin Winslow <awinslow@camsys.com>
2015-09-21 19:06:15 -0400

    Tweak media queries and link menu; add page fold

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
   transformicons.add('.tcon');
-  $('#card2 #floater, #card4 #floater, #card1 #posts, #card3 #bookshelf').hide();
+  $('#card2 #floater, #card4 #floater, #card1 #posts, #card3 #bookshelf, #card4 #floater #links').hide();
 
   $('#navLink').on('click', function(){
     $('#card2 #floater, #card4 #floater, #card1 #posts, #card3 #bookshelf').fadeToggle(1000, function(){});
   });
 
-  $('.switch-blurbs').click(function(){
-    $('#card2 #floater #blurb, #card4 #floater #blurb').fadeToggle(1000, function(){});
-    $('#posts, #card4 #posts, #bookshelf, #card4 #bookshelf').fadeToggle(1000, function(){});
+  $('#card4 #floater').click(function(){
+    if ($(this).find('#blurb').is(':visible')) {
+      $(this).find('#blurb').fadeToggle(1000, function(){});
+      $(this).find('#ohu').fadeToggle(1000, function(){});
+      $(this).find('#links').delay(1000).fadeToggle(1000, function(){});
+    } else {
+      $(this).find('#links').fadeToggle(1000, function(){});
+      $(this).find('#blurb').delay(1000).fadeToggle(1000, function(){});
+      $(this).find('#ohu').delay(1000).fadeToggle(1000, function(){});
+    }
   });
 });
